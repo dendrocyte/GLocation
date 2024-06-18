191c9413679f3894ce89292b4149e7d1d586197d
tina <tina.lu@noovo.co>
2024-06-18 16:50:10 +0800

    Interact with UtilViewActivity and UtilFunctionActivity

--- app/src/main/java/com/example/googlelocation/demo/MainActivity.kt
+++ app/src/main/java/com/example/googlelocation/demo/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.googlelocation.demo
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import cus.yiling.location.R
 import cus.yiling.location.databinding.ActivityMainBinding
-import cus.yiling.location.databinding.ActivityUtilBinding
 
 /**
  * Created by luyiling on 2024/2/26
@@ -30,10 +26,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnNaviUtilBtn.setOnClickListener {
-            startActivity(Intent(baseContext, UtilBtnActivity::class.java))
+            startActivity(Intent(baseContext, UtilViewActivity::class.java))
         }
         binding.btnNonViewUtil.setOnClickListener {
-
+            startActivity(Intent(baseContext, UtilFunctionActivity::class.java))
         }
 
     }
